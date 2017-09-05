4920fc10262073a4ebfa870c1b1df5cd487161f4
Deepak_local_repo <deepak.prabhu@waitrose.co.uk>
2017-09-06 04:00:36 +0530

    hi

---
+++ com.sdetTraining.Maven/src/main/java/com/sdetTraining/com/sdetTraining/Maven/App.java
@@ -0,0 +1,24 @@
+package com.sdetTraining.com.sdetTraining.Maven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+@Test
+public class App 
+{
+	
+	
+    public void myOwnMethod()
+    {
+        System.out.println( "Hello World!" );
+        
+        //This is my update which i like to stage to perform the commit action
+        
+    }
+}
